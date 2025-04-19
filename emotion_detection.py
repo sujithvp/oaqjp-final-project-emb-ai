062a35a3310d1ecb468db7d2449fbc03ff092e65
sujithvp <sujithvpanicker@gmail.com>
2025-04-19 09:53:18 -0400

    second commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,21 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=myobj, headers = headers)
     formatted_response = json.loads(response.text)
-    return formatted_response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = dominant_emotion
+    return emotion_scores
